e2b27fafde0c5bdda398730444045e9e0659872a
Arijit Banerjee <53684453+iamarijit360x@users.noreply.github.com>
2023-02-28 19:55:04 +0530

    Minor fixs

--- Encode.py
+++ Encode.py
@@ -36,14 +36,14 @@ def encode(msg,nob):
     for k in range(len(msg_arr)):
         
         t=img_arr[i][j][rgb]
-        #print(f"{i}{j}{rgb}")
+        print(f"{i}{j}{rgb}")
         t=list(format(t,"b"))
         t=t[-nob:]
         t="".join(t)
         
         c=change(int(t,2),msg_arr[k],bucket[msg_arr[k]],nob)
-        print(f"IMG OLD={img_arr[i][j][rgb]} {format(img_arr[i][j][rgb],'b')}",end=" ")
-        print(f"DATA={msg_arr[k]},ImgPixel={t},chnage Needed={c} " ,end=" ")
+        print(f"IMG OLD={img_arr[i][j]} ",end=" ")
+        #print(f"DATA={msg_arr[k]},ImgPixel={t},chnage Needed={c} " ,end=" ")
         
         if(c!=-1):
             c=list(format(c,'0'+str(nob)+'b'))
@@ -56,8 +56,8 @@ def encode(msg,nob):
             t="".join(t)
             t=t+c
             img_arr[i][j][rgb]=int(t,2)
-        #print(f"DATA NEW={add(img_arr[i][j],nob)} ",end=" ")
-        print(f"  IMG NEW={img_arr[i][j][rgb]}  {format(img_arr[i][j][rgb],'b')}")
+        print(f"DATA NEW={img_arr[i][j]} ",end=" ")
+        #print(f"  IMG NEW={img_arr[i][j][rgb]}  {format(img_arr[i][j][rgb],'b')}")
         rgb+=1
         if(rgb==3):
             j=j+1
